6aed120fa1d36e8fda3ed4bcfadfa6adacb9e32b
John Wang <johncwang@gmail.com>
2016-11-19 15:12:38 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+Broken Website
+==============
+
+This is an example broken website to use for testing purposes.
+
+It currently generates the following conditions:
+
+* High latency web pages
+* Error web pages
+
+## Installation
+
+### Heroku
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+### Usage
+
+```bash
+$ git clone https://github.com/grokify/broken-website
+$ cd broken-website
+$ bundle
+$ NEWRELIC_APP_ID=<MY_APP_ID> NEWRELIC_LICENSE_KEY=<MY_LICENSE_KEY> ruby app.rb
+```
+
+## Latency Testing
+
+### Apdex levels
+
+Apdex tracks three response counts:
+
+* Satisfied: The response time is less than or equal to T.
+* Tolerating: The response time is greater than T and less than or equal to 4T. In this example, 4 x 1.2 = 4.8 seconds as the maximum tolerable response time.
+* Frustrated: The response time is greater than 4T.
+The Time calculation will change based on your own app's T setting. In the following example, T = 1.2 seconds.
+
+| Level | Multiplier | Time (T Example = 1.2) |
+|-------|------------|------------------------|
+| Satisfied | T or less | <= 1.2 seconds |
+| Tolerated | >T, <= 4T | Between 1.2 and 4.8 seconds |
+| Frustrated | > 4T | Greater than 4.8 seconds |
+
+### New Relic
+
+To use this app with New Relic, you need to configure it with your New Relic Browser `licenseKey` and `applicationId`. These two values can be retrieved from the JavaScript in `Application settings` > `Settings`.
+
+To test New Relic alerts, you can use the latency generator with the Apdex T value setting in New Relic.
+
+This is set in `Settings` > `Application Settings`
+
+## Links
+
+Apdex
+
+* https://en.wikipedia.org/wiki/Apdex
---
+++ app.rb
@@ -0,0 +1,95 @@
+#!ruby
+require 'sinatra'
+
+set :port, ENV['PORT']
+set :public_folder, 'public'
+
+newrelic_app_id = ENV['NEWRELIC_APP_ID']
+newrelic_license_key = ENV['NEWRELIC_LICENSE_KEY']
+
+get '/' do
+  send_file File.join(settings.public_folder, 'index.html')
+end
+
+get '/newrelic.js' do
+  content_type 'application/javascript'
+  erb :newrelicjs, locals: {
+    newrelic_license_key: newrelic_license_key,
+    newrelic_app_id: newrelic_app_id}
+end
+
+get '/latency' do
+  sleep_max = 20
+  sleep_links = []
+  1.upto(sleep_max) do |seconds|
+    sleep_links << %Q{<a href="/latency?latency_time=#{seconds}">#{seconds}</a>}
+  end
+
+  sleep_links_html = sleep_links.join(' ')
+  header_html = '<h1>Latency Generator</h1>'
+
+  sleep_time = params[:latency_time].to_i
+  if sleep_time >= 1 && sleep_time <= sleep_max
+    sleep sleep_time
+    header_html = %Q{
+      <h1>Latency Generator: #{sleep_time} seconds</h1>
+      <p>This is an example #{sleep_time} second latency response.</p>
+    }
+  end
+
+  body %Q{
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <script src="./newrelic.js"></script>
+    </head>
+    <body>
+      #{header_html}
+      <hr>
+      <p>Generate Latency</p>
+      <div>
+        #{sleep_links_html}
+      </div>
+      <hr>
+      <p><a href="/">Home</a> | <a href="/latency">Latency</a> | <a href="/error">Error</a></p>
+    </body></html>
+  }
+end
+
+
+get '/error' do
+  error_status = params[:error_status].to_i
+  error_codes = [400, 401, 402, 403, 404, 405, 406, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 421, 422, 423, 424,426, 428, 429, 431, 451, 500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511]
+  error_links = []
+  error_codes.each do |code|
+    error_links << %Q{<a href="/error?error_status=#{code}">#{code}</a>}
+  end
+  errors_links_html = error_links.join(' ')
+  header_html = '<h1>Error Generator</h1>'
+
+  if error_status >= 400 && error_status < 600
+    status error_status
+    header_html = %Q{
+      <h1>Error Generator: #{error_status}</h1>
+      <p>This is an example #{error_status} error.</p>
+    }
+  end
+
+  body %Q{
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <script src="./newrelic.js"></script>
+    </head>
+    <body>
+      #{header_html}
+      <hr>
+      <p>Generate Error</p>
+      <div>
+        #{errors_links_html}
+      </div>
+      <hr>
+      <p><a href="/">Home</a> | <a href="/latency">Latency</a> | <a href="/error">Error</a></p>
+    </body></html>
+  }
+end
