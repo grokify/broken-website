8a7eb500696786c0bb3ee327b14835e7295acef2
John Wang <johncwang@gmail.com>
2016-11-19 17:28:11 -0800

    update style

--- app.rb
+++ app.rb
@@ -19,15 +19,9 @@ get '/newrelic.js' do
 end
 
 get '/latency' do
-  sleep_max = 20
-  sleep_links = []
-  0.upto(sleep_max) do |seconds|
-    sleep_links << %Q{<a href="/latency?latency_time=#{seconds}">#{seconds}</a>}
-  end
-  sleep_links_html = sleep_links.join(' ')
-
-  header_html = '<h1>Latency Generator</h1>'
   sleep_time = params[:latency_time].to_i
+  sleep_max = 20
+  header_html = '<h1>Latency Generator</h1>'
   if sleep_time >= 1 && sleep_time <= sleep_max
     sleep sleep_time
     header_html = %Q{
@@ -35,20 +29,18 @@ get '/latency' do
       <p>This is an example #{sleep_time} second latency response.</p>}
   end
 
+  sleep_links = []
+  0.upto(sleep_max) do |seconds|
+    sleep_links << %Q{<a href="/latency?latency_time=#{seconds}">#{seconds}</a>}
+  end
+
   erb :latency, locals: {
     header_html: header_html,
-    links_html: sleep_links_html}
+    links_html: sleep_links.join(' ')}
 end
 
 get '/error' do
   error_status = params[:error_status].to_i
-  error_codes = [400, 401, 402, 403, 404, 405, 406, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 421, 422, 423, 424,426, 428, 429, 431, 451, 500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511]
-  error_links = []
-  error_codes.each do |code|
-    error_links << %Q{<a href="/error?error_status=#{code}">#{code}</a>}
-  end
-  errors_links_html = error_links.join(' ')
-
   header_html = '<h1>Error Generator</h1>'
   if error_status >= 400 && error_status < 600
     status error_status
@@ -57,7 +49,13 @@ get '/error' do
       <p>This is an example #{error_status} error.</p>}
   end
 
+  error_codes = [400, 401, 402, 403, 404, 405, 406, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 421, 422, 423, 424,426, 428, 429, 431, 451, 500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511]
+  error_links = []
+  error_codes.each do |code|
+    error_links << %Q{<a href="/error?error_status=#{code}">#{code}</a>}
+  end
+
   erb :error, locals: {
     header_html: header_html,
-    links_html: errors_links_html}
+    links_html: error_links.join(' ')}
 end
